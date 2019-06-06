e81e0f1b3afd89b7e1bc1c2c6f44c81ae3c7188b
Austin Mroz <AustinMroz@utexas.edu>
2019-06-06 11:41:11 -0500

    Update version number
    The update to Settings requires a bump of version number to prevent
    incompatibilities. I'm not sure the exact system dabomstew has in place
    for in version numbers, but assuming standard symver, an update to minor
    number is most appropriate

--- src/com/dabomstew/pkrandom/gui/PresetLoadDialog.java
+++ src/com/dabomstew/pkrandom/gui/PresetLoadDialog.java
@@ -208,6 +208,8 @@ public class PresetLoadDialog extends javax.swing.JDialog {
                 versionWanted = "1.7.0b";
             } else if (presetVN == 171) {
                 versionWanted = "1.7.1";
+            } else if (presetVN == 172) {
+                versionWanted = "1.7.2";
             }
             JOptionPane.showMessageDialog(this,
                     String.format(bundle.getString("PresetLoadDialog.olderVersionRequired"), versionWanted));
